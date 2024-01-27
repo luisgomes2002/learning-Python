f1c8292dff5aa2833b6e7c433513987b4553933e
Luis <luisgmgomes@gmail.com>
2024-01-27 00:23:07 -0300

    binary tree

---
+++ binaryTree/app.py
@@ -0,0 +1,10 @@
+from node import Node
+from binary_tree import BinaryTree
+
+tree = BinaryTree(Node(9))
+tree.add(Node(5))
+tree.add(Node(11))
+
+print(tree.head)
+print(tree.head.left)
+print(tree.head.right)
